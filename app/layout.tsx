fb432519d6c1c1d01ce12b4e0ff2b92b6eacf46b
DEVStumz <davidbilewumi51@gmail.com>
2026-04-03 07:00:43 -0700

    Revert to .tsx and fix TypeScript syntax

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import type { Metadata } from "next";
 import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono'; 
 import "./globals.css";
@@ -6,12 +7,16 @@ import "./globals.css";
 const geistSans = GeistSans; 
 const geistMono = GeistMono;
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "SEOtalo",
   description: "Best analytics app for agencies, consultants, affiliates, e-commerce, and more.",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+   children, 
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html
       lang="en"
